51a8e5ae040f35f464c72d6593b9683aea2f8aed
kelsey23 <student12@JS-C02MXF6KG083.local>
2016-07-01 15:02:03 -1000

    apps in progress

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,11 @@
 window.onload = doThings; 
 function doThings(){
-	
+	var lanikai = document.getElementById("lanikai"); 
+	lanikai.addEventListener("click", handleclick); 	
 }
+function handleclick(evt){
+	console.log(evt.target.id);
+}
+
+
+
